d1d4b09b787badf37d3a368113e80375ff7d1299
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2020-07-05 10:49:02 +0100

    Create standalone cache for place view models

---
+++ FindAndExplore/Caches/PlacesCache.cs
@@ -0,0 +1,50 @@
+using System;
+using DynamicData;
+using FindAndExplore.DatasetProviders;
+using FindAndExplore.Infrastructure;
+using FindAndExplore.Reactive;
+using FindAndExplore.ViewModels;
+using ReactiveUI;
+using System.Reactive.Concurrency;
+
+namespace FindAndExplore.Caches
+{
+    public class PlacesCache : ReactiveObject, IPlacesCache
+    {
+        readonly ISchedulerProvider _schedulerProvider;
+        readonly IErrorReporter _errorReporter;
+        readonly IFoursquareDatasetProvider _foursquareDatasetProvider;
+        readonly IFindAndExploreDatasetProvider _findAndExploreDatasetProvider;
+        
+        public SourceCache<PlaceViewModel, String> ViewModelCache { get; }
+        
+        public void Clear()
+        {
+            _schedulerProvider.MainThread.Schedule(() =>
+            {
+                ViewModelCache.Clear();
+            });
+        }
+
+        static readonly Func<PlaceViewModel, string> PlacesKeySelector = place => place.Id;
+        
+        public PlacesCache(
+            ISchedulerProvider schedulerProvider,
+            IErrorReporter errorReporter,
+            IFoursquareDatasetProvider foursquareDatasetProvider,
+            IFindAndExploreDatasetProvider findAndExploreDatasetProvider)
+        {
+            _schedulerProvider = schedulerProvider;
+            _errorReporter = errorReporter;
+            _foursquareDatasetProvider = foursquareDatasetProvider;
+            _findAndExploreDatasetProvider = findAndExploreDatasetProvider;
+
+            ViewModelCache = new SourceCache<PlaceViewModel, string>(PlacesKeySelector);
+            
+            // connect the various DatasetProvider ViewModelCaches to this combined cache
+            _ = _findAndExploreDatasetProvider.ViewModelCache.Connect().PopulateInto(ViewModelCache);
+            _ = _foursquareDatasetProvider.ViewModelCache.Connect().PopulateInto(ViewModelCache);
+            
+        }
+    }
+}
